1fab4edb9d276b5503334fb80c33a9365a8b7f8f
Davide-Resigotti <idavide.resi@gmail.com>
2024-09-26 12:18:38 +0200

    added folder support

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from mutagen.id3 import ID3, TIT2, TPE1, TALB, TPE2, TXXX, TRCK
 import re
+import os
+
+def manage_folder_tags(folder_path):
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith('.mp3'):
+                file_path = os.path.join(root, file)
+                manage_tags(file_path)
 
 def manage_tags(file_path):
     
     audio = ID3(file_path)
     
-    #get tags
+    # ------------------- GET TAGS -------------------
+    
     title = audio.get('TIT2', [None])
     artist = audio.get('TPE1', [None])
     album = audio.get('TALB', [None])
@@ -13,27 +22,27 @@ def manage_tags(file_path):
     artists = audio.getall('TXXX:ARTISTS')[0] if audio.getall('TXXX:ARTISTS') else None
     track_number = audio.get('TRCK', [None])
     
+        
+    if title is None or artist is None:
+        print("no artist or title")
+        return
+    
     # Get the first title to compare to album name to check if it's a single
     first_title = title[0] if title else None
     
     # Get the first artist to compare to artist
     first_artists = artists if artists else None
     
-    
+    print("ORINGAL TAGS")
     print(f"Titolo: {title}")
-    print(f"Artista: {artist}")
-    print(f"Album: {album}")
-    print(f"Album Artist: {albumArtist}")
-    print(f"Artisti: {artists}")
+    print(f"Artista: {artist.text}")
+    print(f"Album: {album}") if album else None
+    print(f"Album Artist: {albumArtist}") if albumArtist else None
+    print(f"Artisti: {artists.text}") if artists else None
     print()
     
     
-    #edit tags
-    
-    if title is None or artist is None:
-        print("no artist or title")
-        return
-
+    # ------------------- EDIT TAGS -------------------
 
     # Split the list of artists
     if artists is None:
@@ -41,7 +50,7 @@ def manage_tags(file_path):
     artists_list = ', '.join(artists).split(', ')
         
     # Get the main artist
-    principal_artist = artists[0]
+    principal_artist = artists[0] if artists else artist[0]
     
     # Remove the main artist from the list of featured artists
     if principal_artist in artists_list:
@@ -50,25 +59,27 @@ def manage_tags(file_path):
     # Create a string with the list of featured artists
     feat_artists = ' & '.join(artists_list)
     
-    
-    
-        
+    print("EDITED TAGS")
     
     # TITLE
     # Check if feat artists are set, if not set it to the list of featured artists
     title = title[0]
-    if "feat" not in title:
+    if "feat" not in title and len(artists_list) > 0:
         new_title = f"{title} (feat. {feat_artists})"
-        print("feat not in title")
-    elif "(feat." in title and title != f"{title} (feat. {feat_artists})":
+        # print("feat not in title")
+    elif "(feat." in title:
         new_title = re.sub(r'\(feat\..*', f"(feat. {feat_artists})", title)
-        print("(feat in title")
+        # print("(feat in title")
     elif "feat." in title:
         new_title = re.sub(r'\feat\..*', f"(feat. {feat_artists})", title)
-        print("feat. in title")
+        # print("feat. in title")
+    else:
+        new_title = title
 
-    audio['TIT2'] = TIT2(encoding=3, text=new_title)
-    print(f"Titolo aggiornato: {new_title}")
+    # Update the title tag
+    if title != new_title:
+        audio['TIT2'] = TIT2(encoding=3, text=new_title)
+        print(f"Titolo aggiornato: {new_title}")
     
     #ARTIST
     # Check if artist is set, if not set it to the principal artist
@@ -80,7 +91,9 @@ def manage_tags(file_path):
     # Chek if the track is a single, if so set the album name to singles
     if first_title == album:
         audio['TALB'] = TALB(encoding=3, text='singles')
+        album = 'singles'
         print("Album impostato a singles")
+
         
     #TRACK NUMBER
     # Check if track number is set, if not set it to 1
@@ -100,13 +113,27 @@ def manage_tags(file_path):
     if first_artists is None:
         artists_list.insert(0, principal_artist)
         audio['TXXX:ARTISTS'] = TXXX(encoding=3, text=artists_list, desc='ARTISTS')
-        print(f"Artisti aggiornati: {audio['TXXX:ARTISTS']}")
+        print(f"Artisti aggiornati: {audio['TXXX:ARTISTS'].text}")
+        
+    #DISC NUMBER
+    # remove disk number for singles
+    if album == 'singles':
+        audio.delall('TPOS')
+    
   
+    print()
+    print("-----------------------------")
+    print()
         
     audio.save()
     
-
-  
+    # Update the file name with the new title if it isn't already correct
+    new_file_name = re.sub(r'.*\.mp3$', f"{new_title}.mp3", os.path.basename(file_path))
+    new_file_path = os.path.join(os.path.dirname(file_path), new_file_name)
+    if file_path != new_file_path:
+        os.rename(file_path, new_file_path)
+        file_path = new_file_path
+    
  
 def print_tags(file_path):
     try:
@@ -117,12 +144,10 @@ def print_tags(file_path):
     except Exception as e:
         print(f"Errore nel caricamento del file: {e}")
         
-        
-
 
 
 if __name__ == "__main__":
-    file_path = '/Users/davideresigotti/Desktop/03 Emoji della traphouse.mp3'
+    folder_path = '/Users/davideresigotti/Downloads/Going Hard'
   
-    # print_tags(file_path)
-    manage_tags(file_path)
+    manage_folder_tags(folder_path)
+
